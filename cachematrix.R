78fc9f561c8857afbb32daca2a3343b4796c9b84
dottie333 <dorothyp533@gmail.com>
2014-07-17 00:29:14 -0500

    changes to cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program creates two functions: makeCacheMatrix and cacheSolve.  
+## The makeCacheMatrix creates a "matrix" object that can cache its inverse.  
+## CacheSolve computes the inverse of the matrix makeCacheMatrix. 
 
-## Write a short comment describing this function
+## This function will take a matrix object as an input to the makeCacheMatrix function.   
+## The matrix input must be a square matrix.   
+## The get() function is used to retrieve the format of the Matrix.  
+## After running the function script, from the command line I executed the following statements:
+##
+#       > v <- makeCacheMatrix(matrix(c(1,2,3,4),nrow =2))
+#       > v$get()
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+        
+    }
+    get <- function() x
+    setmatrix <- function(matrix) m <<- matrix
+    getmatrix <- function() m
+    list(set = set, get = get,
+         setmatrix = setmatrix,
+         getmatrix = getmatrix)  
+    
 }
+    
+##  This function cacheSolve will create the inversion of the matrix
+##  created by the makeCacheMatrix function. 
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getmatrix()
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setmatrix(m)
+    m
+
+        ## To return the inverse of the function
+        ## From the command line, I executed the fllowing statment.
+        ##  > cacheSolve(v)
+    
 }
